f6ee087812f38ed00468a0997153a0a9d284e0bc
nhemery <nhemery@ST203>
2022-09-27 09:26:26 +0200

    Ajoutez des fichiers projet.

---
+++ GSB_FSociety/FMenu.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace GSB_FSociety
+{
+    partial class FMenu
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonMission1 = new System.Windows.Forms.Button();
+            this.buttonMission2 = new System.Windows.Forms.Button();
+            this.buttonMission3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonMission1
+            // 
+            this.buttonMission1.Location = new System.Drawing.Point(62, 28);
+            this.buttonMission1.Name = "buttonMission1";
+            this.buttonMission1.Size = new System.Drawing.Size(75, 23);
+            this.buttonMission1.TabIndex = 5;
+            this.buttonMission1.Text = "Mission 1";
+            this.buttonMission1.UseVisualStyleBackColor = true;
+            this.buttonMission1.Click += new System.EventHandler(this.buttonMission1_Click);
+            // 
+            // buttonMission2
+            // 
+            this.buttonMission2.Location = new System.Drawing.Point(62, 67);
+            this.buttonMission2.Name = "buttonMission2";
+            this.buttonMission2.Size = new System.Drawing.Size(75, 23);
+            this.buttonMission2.TabIndex = 6;
+            this.buttonMission2.Text = "Mission 2";
+            this.buttonMission2.UseVisualStyleBackColor = true;
+            this.buttonMission2.Click += new System.EventHandler(this.buttonMission2_Click);
+            // 
+            // buttonMission3
+            // 
+            this.buttonMission3.Location = new System.Drawing.Point(62, 106);
+            this.buttonMission3.Name = "buttonMission3";
+            this.buttonMission3.Size = new System.Drawing.Size(75, 23);
+            this.buttonMission3.TabIndex = 7;
+            this.buttonMission3.Text = "Mission 3";
+            this.buttonMission3.UseVisualStyleBackColor = true;
+            this.buttonMission3.Click += new System.EventHandler(this.buttonMission3_Click);
+            // 
+            // FMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(214, 161);
+            this.Controls.Add(this.buttonMission3);
+            this.Controls.Add(this.buttonMission2);
+            this.Controls.Add(this.buttonMission1);
+            this.Name = "FMenu";
+            this.Text = "FMenu";
+            this.Load += new System.EventHandler(this.FMenu_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonMission1;
+        private System.Windows.Forms.Button buttonMission2;
+        private System.Windows.Forms.Button buttonMission3;
+    }
+}
+
---
+++ GSB_FSociety/FMenu.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GSB_FSociety
+{
+    public partial class FMenu : Form
+    {
+        public FMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void FMenu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonMission1_Click(object sender, EventArgs e)
+        {
+            FM1 fM1 = new FM1();
+            fM1.ShowDialog();
+            this.Close();
+        }
+
+        private void buttonMission2_Click(object sender, EventArgs e)
+        {
+            FM2 fM2 = new FM2();
+            fM2.ShowDialog();
+            this.Close();
+        }
+
+        private void buttonMission3_Click(object sender, EventArgs e)
+        {
+            FM3 fM3 = new FM3();
+            fM3.ShowDialog();
+            this.Close();
+        }
+    }
+}
