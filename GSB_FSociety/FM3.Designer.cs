e28c003e47c67885da81ce1a09b4ac1b1ba2bdbe
otiryk <kyrito35@gmail.com>
2022-09-27 11:52:03 +0200

    base du FM3

--- GSB_FSociety/FM3.Designer.cs
+++ GSB_FSociety/FM3.Designer.cs
@@ -19,7 +19,6 @@
             }
             base.Dispose(disposing);
         }
-
         #region Windows Form Designer generated code
 
         /// <summary>
@@ -28,45 +27,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.test = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // test
-            // 
-            this.test.Location = new System.Drawing.Point(370, 136);
-            this.test.Name = "test";
-            this.test.Size = new System.Drawing.Size(75, 23);
-            this.test.TabIndex = 0;
-            this.test.Text = "test";
-            this.test.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(534, 290);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
             // 
             // FM3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.test);
             this.Name = "FM3";
             this.Text = "FM3";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button test;
-        private System.Windows.Forms.Label label1;
     }
 }
