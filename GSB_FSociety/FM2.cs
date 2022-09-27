e4e448cb851e334441f5dc61313b024ad5b34708
Styann <steunouyannpro@gmail.com>
2022-09-27 10:50:13 +0200

    test

--- GSB_FSociety/FM2.cs
+++ GSB_FSociety/FM2.cs
@@ -16,5 +16,15 @@ namespace GSB_FSociety
         {
             InitializeComponent();
         }
+
+        private void FM2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
