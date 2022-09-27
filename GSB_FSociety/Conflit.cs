ad0d6efd93216b6af63afef595a6b3b234fc5a09
Styann <steunouyannpro@gmail.com>
2022-09-27 11:38:05 +0200

    test conflit

---
+++ GSB_FSociety/Conflit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSB_FSociety
+{
+    internal class Conflit
+    {
+/*
+ * ligne1
+ * l2
+ * l3
+ * l4
+ * l5
+ * l6
+ */
+    }
+}
